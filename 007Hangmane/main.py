45c9224766fba60b6ec602ffbfd6fd0a3b8a1206
Michael Ly <71558719+Michael-Q-Ly@users.noreply.github.com>
2024-04-30 16:19:03 -0500

    Generate a random word and check each letter as right or wrong

--- 007Hangmane/main.py
+++ 007Hangmane/main.py
@@ -1,17 +1,22 @@
-#Step 1 
+"""
+Hangman Game
+"""
+import random
+
+# Step 1
 
 word_list = ["aardvark", "baboon", "camel"]
 
-#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
-import random
+# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 chosen_word = random.choice(word_list)
 
-#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = input("Guess a letter: ").lower()
+# TODO-2 - Ask the user to guess a letter and assign their answer to a variable
+# called guess. Make guess lowercase.
+guess = input("Input a letter: ").lower()
 
-#TODO-3 - Check if the letter the user guessed (guess) is one of the leters in the chosen_word.
+# TODO-3 - Check if the letter the user guessed (guess) is one of the leters in the chosen_word.
 for letter in chosen_word:
-    if letter == guess:
-        print("Right")
+    if guess == letter:
+        print("Correct")
     else:
         print("Wrong")
