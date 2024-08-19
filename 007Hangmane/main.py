2c8b494ce43fbad9378c6b85776b791871e4baf6
Michael Ly <71558719+Michael-Q-Ly@users.noreply.github.com>
2024-08-18 22:46:59 -0500

    Day 7: Update hangman game to replace blanks with guesses

--- 007Hangmane/main.py
+++ 007Hangmane/main.py
@@ -7,16 +7,45 @@ import random
 
 word_list = ["aardvark", "baboon", "camel"]
 
-# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 chosen_word = random.choice(word_list)
+print(chosen_word)
 
-# TODO-2 - Ask the user to guess a letter and assign their answer to a variable
-# called guess. Make guess lowercase.
+"""
+
+# TODO-1: Create a "placeholder" with the same number of blanks as letters
+blank_list = []
+for i in range(len(chosen_word)):
+    blank_list.append('_')
+print(*blank_list, sep='')
+
 guess = input("Input a letter: ").lower()
 
-# TODO-3 - Check if the letter the user guessed (guess) is one of the leters in the chosen_word.
+# TODO-2: Create a "display" that puts the guess letter in the correct position
+# in the blanks
+
+for i in range(len(chosen_word)):
+    if guess == chosen_word[i]:
+        blank_list[i] = guess
+        print(*blank_list, sep='')
+"""
+
+# TODO-1: Create a "placeholder" with the same number of blanks as letters
+placeholder = ''
+for position in range(len(chosen_word)):
+    placeholder += '_'
+print(placeholder)
+
+guess = input("Input a letter: ").lower()
+
+# TODO-2: Create a "display" that puts the guess letter in the correct position
+# in the blanks
+
+display = ''
+position = 0
 for letter in chosen_word:
     if guess == letter:
-        print("Correct")
+        display += letter
     else:
-        print("Wrong")
+        display += '_'
+
+print(display)
