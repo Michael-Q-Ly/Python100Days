ab36fd5120b2e1850db938ed6d31595616d5af7c
Michael Ly <71558719+Michael-Q-Ly@users.noreply.github.com>
2024-08-24 16:23:33 -0500

    Day 7 - Complete with additional features
    	- Do not decrease life for repeated incorrect letter
    	- Warn user of any repeated letter
    	- Split project into separate user modules
    	- Show how many lives left
    	- Let user know what word is if they did not win

--- PyCharm/Day 7/task/main.py
+++ PyCharm/Day 7/task/main.py
@@ -1,13 +1,17 @@
+import hangman_words
+import hangman_art
 import random
 
 # TODO-1: - Update the word list to use the 'word_list' from hangman_words.py
-
+word_list = hangman_words.word_list
 lives = 6
 
 # TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.
+logo = hangman_art.logo
+print(logo)
+stages = hangman_art.stages
 
 chosen_word = random.choice(word_list)
-print(chosen_word)
 
 placeholder = ""
 word_length = len(chosen_word)
@@ -17,14 +21,18 @@ print("Word to guess: " + placeholder)
 
 game_over = False
 correct_letters = []
+incorrect_letters = []
 
 while not game_over:
 
     # TODO-6: - Update the code below to tell the user how many lives they have left.
-    print("****************************<???>/6 LIVES LEFT****************************")
+    print(f"****************************<{lives}>/{len(stages)-1} LIVES LEFT****************************")
     guess = input("Guess a letter: ").lower()
 
     # TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
+    if (guess in incorrect_letters) or (guess in correct_letters):
+        print("You have already guessed this letter!")
+        continue
 
     display = ""
 
@@ -44,12 +52,14 @@ while not game_over:
 
     if guess not in chosen_word:
         lives -= 1
+        incorrect_letters.append(guess)
+        print(f"You guessed {guess}. That is not in the word. you lose a life.")
 
         if lives == 0:
             game_over = True
 
             # TODO 7: - Update the print statement below to give the user the correct word they were trying to guess.
-            print(f"***********************YOU LOSE**********************")
+            print(f"***********************It was {chosen_word}! YOU LOSE**********************")
 
     if "_" not in display:
         game_over = True
