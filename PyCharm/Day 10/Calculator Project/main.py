24f8fca77b60768bf928ae761fd732901044c8e6
Michael Ly <71558719+Michael-Q-Ly@users.noreply.github.com>
2024-10-12 22:01:42 -0600

    Day 10: Calculator - Add subtract, multiply, and divide functions

--- PyCharm/Day 10/Calculator Project/main.py
+++ PyCharm/Day 10/Calculator Project/main.py
@@ -1,4 +1,18 @@
 def add(n1, n2):
+    """Adds two numbers together"""
     return n1 + n2
 
 
+def subtract(n1, n2):
+    """Subtracts the second number from the first number"""
+    return n1 - n2
+
+
+def multiply(n1, n2):
+    """Multiples two numbers together"""
+    return n1 * n2
+
+
+def divide(n1, n2):
+    """Divides the first number by the second number"""
+    return n1 / n2
